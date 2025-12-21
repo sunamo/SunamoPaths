946322b2016edf250b4f7315944c38d1e777b0ff
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:26:12 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoPaths/DefaultPathsData.cs
+++ SunamoPaths/DefaultPathsData.cs
@@ -1,5 +1,3 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 namespace SunamoPaths;
 
 public partial class DefaultPaths
